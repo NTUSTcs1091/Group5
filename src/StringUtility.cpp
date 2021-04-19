e90bff9f3e1a474d1d1e7cc8e28114404b5def11
KungYao <masa10269@gmail.com>
2021-04-19 18:07:59 +0800

    add string utility header

---
+++ src/StringUtility.cpp
@@ -0,0 +1,8 @@
+// Copyright (c) 2021, Kung-Yao Hsieh, Shang-Chen LIN, Yi-Hang Xie. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "StringUtility.h"
+
+#include <boost/algorithm/string/split.hpp>
+#include <boost/algorithm/string/classification.hpp>
---
+++ src/StringUtility.h
@@ -0,0 +1,37 @@
+// Copyright (c) 2021, Kung-Yao Hsieh, Shang-Chen LIN, Yi-Hang Xie. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef HTTPLIB_SRC_STRINGUTILITY_H
+#define HTTPLIB_SRC_STRINGUTILITY_H
+
+#include <vector>
+#include <string>
+
+/** @brief String utility function */
+namespace StringUtility {
+    /** @brief Split input string into tokens by use of delimiters.
+     *
+     *  This method is equivalent to:
+     *      std::vector<std::string> token(0);
+     *      boost::split(token, str, boost::is_any_of(dels), boost::token_compress_on);
+     *      return token;
+     *
+     *  @param str Input string.
+     *  @param dels Characters that seperate string.
+     *  @return Result tokens.
+     */
+    std::vector<std::string> split(const std::string& str, const std::string& dels);
+    /** @brief Generate SHA1 hash of the string.
+     *  @param str Input string.
+     *  @return SHA1 hash of input string.
+     */
+    std::string SHA1(const std::string& str);
+    /** @brief Encode string into BASE64.
+     *  @param str Input string.
+     *  @return BASE64 string.
+     */
+    std::string encodeBASE64(const std::string& str);
+};
+
+#endif
