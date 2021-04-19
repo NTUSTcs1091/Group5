3cad53390a96e78a7a128c369afc9b8d95209e65
Geas <miku870725@gmail.com>
2021-04-19 18:54:48 +0800

    Add HttpSession

---
+++ src/HttpSession.cpp
@@ -0,0 +1,5 @@
+// Copyright (c) 2021,Kung-Yao Hsieh, Shang-Chen LIN, Yi-Hang Xie. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "HttpSession.h"
