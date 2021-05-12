531145cdd1eb401b43f48534cea00565bd4cddca
KungYao <masa10269@gmail.com>
2021-05-12 16:00:50 +0800

    implement WebServer

--- src/WebServer.cpp
+++ src/WebServer.cpp
@@ -3,3 +3,15 @@
 // found in the LICENSE file.
 
 #include "WebServer.h"
+
+#include <boost/asio.hpp>
+
+WebServer::WebServer(const std::string& ip, const int& port) {
+    listener_ = std::make_unique<HttpListener>(&ioc_, ip, port);
+}
+
+void WebServer::run() {
+    listener_->runAsync();
+    // project will be block at here until all objects using ioc are close.
+    ioc_.run();
+}
