0eb64a1e70968ff94ac4fc75b7a292a65eb4699d
KungYao <masa10269@gmail.com>
2021-05-12 15:09:02 +0800

    update function name

--- src/StringUtility.cpp
+++ src/StringUtility.cpp
@@ -9,7 +9,6 @@
 #include <openssl/bio.h>
 #include <openssl/buffer.h>
 #include <openssl/evp.h>
-#include <openssl/sha.h>
 
 namespace StringUtility {
     std::vector<std::string> split(const std::string& str, const std::string& dels) {
@@ -20,9 +19,18 @@ namespace StringUtility {
     }
 
     std::string sha1(const std::string& str) {
-        unsigned char output_buf[20];
-        SHA1((unsigned char*)str.c_str(), str.length(), output_buf);
-        std::string result((char *)output_buf, 20);
+        const EVP_MD* md = EVP_sha1();
+        EVP_MD_CTX* mdctx = EVP_MD_CTX_create();
+        unsigned int output_length = 0;
+        unsigned char output_buf[EVP_MAX_MD_SIZE];
+
+        EVP_DigestInit_ex(mdctx, md, NULL);
+        EVP_DigestUpdate(mdctx, str.c_str(), str.length());
+        EVP_DigestFinal_ex(mdctx, output_buf, &output_length);
+        EVP_MD_CTX_destroy(mdctx);
+        EVP_cleanup();
+
+        std::string result((char *)output_buf, output_length);
         return result;
     }
 
@@ -42,6 +50,8 @@ namespace StringUtility {
         BIO_get_mem_ptr(b64, &bptr);
 
         std::string result(bptr->data, bptr->length);
+        // According to OpenSSl API document, we do not need to free *bmem because
+        // this function will free entire BIO chain. 
         BIO_free_all(b64);
 
         return result;
