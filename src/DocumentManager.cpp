aeabff0f863bd0058ef9c7a00649f256718a8a05
Hanakuru <lead8964878@yahoo.com.tw>
2021-06-27 01:33:03 +0800

    implement document manager

--- src/DocumentManager.cpp
+++ src/DocumentManager.cpp
@@ -3,3 +3,28 @@
 // found in the LICENSE file.
 
 #include "DocumentManager.h"
+
+void DocumentManager::updateDocumentByID(const int id, Revision revision)
+{
+    if (doc_map.find(id) != doc_map.end())
+    {
+        doc_map.find(id)->second.history.push_back(revision);
+    }
+    else
+    {
+        Document doc(id, std::vector<Revision>());
+        doc_map.insert(std::pair<int, Revision>(id, doc));
+    }
+}
+
+std::vector<Revision> *DocumentManager::getHistoryFromDocument(const int id)
+{
+    if (doc_map.find(id) != doc_map.end())
+    {
+        return &doc_map.find(id)->second.history;
+    }
+    else
+    {
+        return nullptr;
+    }
+}
