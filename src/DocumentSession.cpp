8593fc2d937e8db6b41ce7d8a3920581e94a621b
Hanakuru <lead8964878@yahoo.com.tw>
2021-06-30 22:57:33 +0800

    implement document session

--- src/DocumentSession.cpp
+++ src/DocumentSession.cpp
@@ -3,3 +3,63 @@
 // found in the LICENSE file.
 
 #include "DocumentSession.h"
+
+DocumentSession::DocumentSession()
+{
+    doc_manager_ = new DocumentManager();
+}
+
+void DocumentSession::addSession(SocketSession *session, const int id)
+{
+    sessions_.insert(session);
+    std::vector<Revision> *revisions_ = doc_manager_->getHistoryFromDocument(id);
+    if (revisions_ != nullptr)
+    {
+        for (const auto &revision : *revisions_)
+        {
+            session->sendMessage(SocketUtility::parseMessage(revision.toString()));
+        }
+    }
+}
+
+void DocumentSession::removeSession(SocketSession *session)
+{
+    sessions_.erase(session);
+}
+
+void DocumentSession::parseUpdateParameters(const std::string &update_params)
+{
+    std::string update_params_ = update_params;
+    std::string delimiter = ",";
+
+    size_t pos = 0;
+    std::string token;
+    std::vector<std::string> param_list;
+    while ((pos = update_params_.find(delimiter)) != std::string::npos)
+    {
+        token = update_params_.substr(0, pos);
+        param_list.push_back(token);
+        update_params_.erase(0, pos + delimiter.length());
+    }
+    param_list.push_back(update_params_);
+    if (param_list.size() == 7)
+    {
+        Revision revision(param_list[1], std::stoi(param_list[2]), std::stoi(param_list[3]),
+                          param_list[4] == "0" ? false : true, param_list[5], std::stoi(param_list[6]));
+        doc_manager_->updateDocumentByID(std::stoi(param_list[0]), revision);
+    }
+}
+
+void DocumentSession::syncDocument(const std::string &update_params)
+{
+    for (const auto &session : sessions_)
+    {
+        session->sendMessage(SocketUtility::parseMessage(update_params));
+    }
+}
+
+void DocumentSession::updateDocument(const std::string &update_params)
+{
+    parseUpdateParameters(update_params);
+    syncDocument();
+}
--- src/DocumentSession.h
+++ src/DocumentSession.h
@@ -10,44 +10,56 @@
 #include <set>
 #include <string>
 
+#include "DocumentManager.h"
+#include "SocketSession.h"
+
 class DocumentManager;
 class SocketSession;
 
 /** @brief Manage the operation between SocketSession and DocumentManager. */
-class DocumentSession {
+class DocumentSession
+{
 private:
     /** @brief Store DocumentManager pointer. It is used to synchronize datas. */
-    DocumentManager* doc_manager_;
+    DocumentManager *doc_manager_;
     /** @brief Store SocketSession object pointer. */
-    std::set<SocketSession*> sessions_;
+    std::set<SocketSession *> sessions_;
     /** @brief Lock sessions_ when it is used. */
     std::mutex session_mutex_;
     /** @brief Parse update parameter reveived from client message.
      *  @param update_params Client message.
      */
-    void parseUpdateParameters(const std::string& update_params);
+    void parseUpdateParameters(const std::string &update_params);
     /** @brief Syncronize the updates to clients and database. */
-    void syncDocument();
+    void syncDocument(const std::string &update_params);
+
 public:
     DocumentSession();
     /** @brief Non construction-copyable. */
-    DocumentSession(const DocumentSession&) = delete;
+    DocumentSession(const DocumentSession &) = delete;
     /** @brief Non copyable. */
-    DocumentSession &operator=(const DocumentSession&) = delete;
+    DocumentSession &operator=(const DocumentSession &) = delete;
     /** @brief Add session after socket connect.
      *  @param session SocketSession object that will be added to session list.
+     *  @param session Document id the SocketSession will be editing.
      */
-    void addSession(SocketSession* session);
+    void addSession(SocketSession *session, const int id);
     /** @brief Remove session after socket close.
      *  @param session SocketSession object that will be removed from session list.
      */
-    void removeSession(SocketSession* session);
+    void removeSession(SocketSession *session);
     /** @brief Update document from client message. Then, function will broadcast to all clients who is editing this document.
      *  @param update_params Client message. The update_params format is like :
      * "{
-     *      "index" : int, 
-     *      "content" : string
+     *      "document_id" : int, 
+     *      "user_id" : string,
+     *      "start_position" : int,
+     *      "end_position" : int,
+     *      "is_insert" : bool,
+     *      "insert_data" : string,
+     *      "delete_length" : int,
      * }".
+     *  @brief For example : "3,user_01,2,5,1,hello_world,0"
      */
     void updateDocument(const std::string &update_params);
 };
